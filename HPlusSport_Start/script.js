529d54d9f9451648a4105f1ac9f5ef107600b65a
kwolfski <kt.schramm.design@gmail.com>
2017-11-07 21:47:10 -0600

    Errors
    I keep getting errors when I am writing code in my JS file, but if I
    paste the code from my script.js doc to my index.html, there are no
    codes…

--- HPlusSport_Start/script.js
+++ HPlusSport_Start/script.js
@@ -1,5 +1,21 @@
 //following the solution video for Lynda.com challenge
- 
+//added 10.1 & 10.2 exercise js
+ $(function () {
+            $("#products h2.product-name[data-type='mineralwater']").css("background-color", "#2982D0");
+            $("#products h2.product-name[data-type='vitamin']").css("background-color", "#12500F");
+            $("#products h2.product-name[data-type='proteinbar']").css("background-color", "#4E0F50");
+
+            document.querySelector('#cbVitamins').addEventListener('change',function (evt) {
+                updateProductView("vitamins", evt.target.checked);
+            });
+            document.querySelector('#cbMineralWater').addEventListener('change',function (evt) {
+                updateProductView("mineralwater", evt.target.checked);
+            });
+            document.querySelector('#cbProtein').addEventListener('change',function (evt) {
+                updateProductView("proteinbar", evt.target.checked);
+            });
+
+
 
 $("product-item").each(function () {
     var prodName = 
@@ -10,3 +26,5 @@ $("product-item").each(function () {
     var link = $("<aref='product.html?prodName=" + prodName + "$prodID=" + prodID + "'/>");
     $(this).children("img").wrap(link);    
 })
+     
+             })
