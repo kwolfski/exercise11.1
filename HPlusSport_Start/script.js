6e7650699b2c4116beb0aa9ba5747fd0a756a098
kwolfski <kt.schramm.design@gmail.com>
2017-11-07 21:43:25 -0600

    step 1

--- HPlusSport_Start/script.js
+++ HPlusSport_Start/script.js
@@ -1,1 +1,12 @@
 //following the solution video for Lynda.com challenge
+ 
+
+$("product-item").each(function () {
+    var prodName = 
+        encodeURIComponent($(this).children("h2").text());
+    var prodID = 
+        encodeURIComponent($(this).data("prod_id"));
+    
+    var link = $("<aref='product.html?prodName=" + prodName + "$prodID=" + prodID + "'/>");
+    $(this).children("img").wrap(link);    
+})
